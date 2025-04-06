99ee8e09b7c98a2c6a29d462ef792c56ab1439ec
Petr <ludaholodecqwerty@gmail.com>
2025-04-06 16:28:49 +0400

    Обработка данных карт и счетов

---
+++ main.py
@@ -0,0 +1,2 @@
+from src.widget import mask_account_card
+print(mask_account_card("Visa Classic 6831982476737658"))
---
+++ src/widget.py
@@ -0,0 +1,35 @@
+def mask_account_card(info: str ) -> str:
+    """ФУнкция обработки """
+
+    parts = info.split(' ')
+    if len(parts) >3:
+        return('Проверьте правильность ввода')
+
+
+    if len(parts) == 3:
+        type_one, type_two, number = parts
+        masked_number = f"{type_one} {type_two} {number[:4]} {number[4:6]}** **** {number[-4:]}"
+    elif len(parts) == 2:
+        type_, number = parts
+        if type_.lower() == "счет":
+             masked_number = f"Счет **{number[-4:]}"
+        else:
+            masked_number =  f"{type_} {number[:4]} {number[4:6]}** **** {number[-4:]}"
+    return (masked_number)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
